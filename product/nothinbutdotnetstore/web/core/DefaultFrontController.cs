d725e181a0e9b52809cef4c8ff03ce4db465af3b
Ronan Moriarty <ronan.moriarty@hotmail.co.uk>
2011-03-16 12:03:37 +0000

    Pushing new changes

--- product/nothinbutdotnetstore/web/core/DefaultFrontController.cs
+++ product/nothinbutdotnetstore/web/core/DefaultFrontController.cs
@@ -4,9 +4,17 @@ namespace nothinbutdotnetstore.web.core
 {
     public class DefaultFrontController : FrontController
     {
-        public void process(Request request)
-        {
-            throw new NotImplementedException();
-        }
+    	readonly CommandRegistry command_registry;
+
+    	public DefaultFrontController(CommandRegistry commandRegistry)
+    	{
+    		command_registry = commandRegistry;
+    	}
+
+    	public void process(Request request)
+    	{
+    		RequestCommand command = command_registry.get_the_command_that_can_handle(request);
+			command.process(request);
+    	}
     }
 }
