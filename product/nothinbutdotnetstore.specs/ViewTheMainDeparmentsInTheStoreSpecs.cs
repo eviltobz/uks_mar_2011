6e2838c38fbcecb2944cd416dea939ecce5ead5d
Dimitri Holsteens <dimitri.holsteens@gmail.com>
2011-03-16 14:06:07 +0000

    Pushing new changes

--- product/nothinbutdotnetstore.specs/ViewTheMainDeparmentsInTheStoreSpecs.cs
+++ product/nothinbutdotnetstore.specs/ViewTheMainDeparmentsInTheStoreSpecs.cs
@@ -1,6 +1,6 @@
+using developwithpassion.specifications.extensions;
 using Machine.Specifications;
 using developwithpassion.specifications.rhino;
-using developwithpassion.specifications.extensions;
 using nothinbutdotnetstore.web.application.catalogbrowsing;
 using nothinbutdotnetstore.web.core;
 
@@ -17,15 +17,42 @@ namespace nothinbutdotnetstore.specs
         [Subject(typeof(ViewTheMainDepartmentsInTheStore))]
         public class when_run : concern
         {
+            Establish e = () =>
+            {
+                department_repository = the_dependency<DepartmentRepository>();
+
+                the_list_of_departments = new string[]{};
+
+                department_repository.setup(x => x.get_all_departments()).
+                    Return(the_list_of_departments);
+
+                rendering_gateway =
+
+            };
 
             Because b = () =>
                 sut.process(request);
 
-            It first_observation = () => { };
+            It should_get_list_of_departments_of_the_departments_repository = () =>
+            {
+                department_repository.received(x => x.get_all_departments());
+            };
+
+            It should_pass_the_list_to_the_result_rendering_gateway = () =>
+            {
+                rendering_gateway.received(x => x.render_response_for(request).using(the_list_of_departments))
+                ;
+            }
 
 
-            static Request request;        
-                
+            static Request request;
+            static DepartmentRepository department_repository;
+            static string[] the_list_of_departments;
         }
+    }
+
+    interface DepartmentRepository
+    {
+        string[] get_all_departments();
     }
 }
