9260b3958430d59ea43dc845c82a1722b3a8be2b
Dimitri Holsteens <dimitri.holsteens@gmail.com>
2011-03-16 13:22:44 +0000

    Pushing new changes

--- product/nothinbutdotnetstore/web/core/DefaultRequestCommand.cs
+++ product/nothinbutdotnetstore/web/core/DefaultRequestCommand.cs
@@ -4,9 +4,16 @@ namespace nothinbutdotnetstore.web.core
 {
     public class DefaultRequestCommand : RequestCommand
     {
+        readonly RequestCriteria request_criteria;
+
+        public DefaultRequestCommand(RequestCriteria request_criteria)
+        {
+            this.request_criteria = request_criteria;
+        }
+
         public bool can_handle(Request request)
         {
-            throw new NotImplementedException();
+            return request_criteria(request);  
         }
 
         public void process(Request request)
