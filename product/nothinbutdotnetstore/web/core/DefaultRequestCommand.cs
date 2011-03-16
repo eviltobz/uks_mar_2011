3b9111c0fc3d4e239a71b924e5221abb8315e2c0
Toby Carter <eviltobz@hotmail.com>
2011-03-16 13:23:28 +0000

    Pushing new changes

--- product/nothinbutdotnetstore/web/core/DefaultRequestCommand.cs
+++ product/nothinbutdotnetstore/web/core/DefaultRequestCommand.cs
@@ -4,9 +4,16 @@ namespace nothinbutdotnetstore.web.core
 {
     public class DefaultRequestCommand : RequestCommand
     {
+        RequestCriteria _criteria;
+
+        public DefaultRequestCommand(RequestCriteria criteria)
+        {
+            _criteria = criteria;
+        }
+
         public bool can_handle(Request request)
         {
-            throw new NotImplementedException();
+            return _criteria(request);
         }
 
         public void process(Request request)
