4d552a6f58cac44ddc45f17f0b16a55d85edc51b
Toby Carter <eviltobz@hotmail.com>
2011-03-17 21:10:45 +0000

    Pushing new changes

---
+++ product/nothinbutdotnetstore.specs/UrlTokenStoreSpecs.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using developwithpassion.specifications.rhino;
+using Machine.Specifications;
+using nothinbutdotnetstore.web.core;
+
+namespace nothinbutdotnetstore.specs
+{
+    public class UrlTokenStoreSpecs
+    {
+        public abstract class concern : Observes<TokenStore, DefaultTokenStore>
+        {
+        }
+
+
+        public class when_adding_tokens_to_store : concern
+        {
+            Establish c = () =>
+            {
+                first = new KeyValuePair<string, object>();
+                second = new KeyValuePair<string, object>();
+            };
+
+            Because b = () =>
+            {
+                sut.add(first);
+                sut.add(second);
+            };
+
+            It should_return_those_items_in_order_they_were_added_so_that_the_url_builder_will_result_in_a_search_engine_friendly_url = () =>
+            {
+                IEnumerator<KeyValuePair<string, object>> enumerator = sut.GetEnumerator();
+                enumerator.MoveNext();
+                enumerator.Current.ShouldEqual(first);
+                enumerator.MoveNext();
+                enumerator.Current.ShouldEqual(second);
+           };
+
+            static KeyValuePair<string, object> first;
+            static KeyValuePair<string, object> second;
+        }
+    }
+
+    
+}
---
+++ product/nothinbutdotnetstore/web/core/DefaultTokenStore.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace nothinbutdotnetstore.web.core
+{
+    public class DefaultTokenStore : TokenStore
+    {
+        public IEnumerator<KeyValuePair<string, object>> GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void add(KeyValuePair<string, object> token_pair)
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
