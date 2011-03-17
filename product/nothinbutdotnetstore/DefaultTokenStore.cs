1ba701cb3f412adc3ea8ac806cd40d970ffacd56
Dimitri Holsteens <dimitri.holsteens@gmail.com>
2011-03-17 21:11:05 +0000

    Pushing new changes

---
+++ product/nothinbutdotnetstore/DefaultTokenStore.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using nothinbutdotnetstore.web.core;
+
+namespace nothinbutdotnetstore
+{
+    public class DefaultTokenStore:TokenStore
+    {
+
+        public IEnumerator<KeyValuePair<string, object>> GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void add(KeyValuePair<string, object> token_pair)
+        {
+            throw new NotImplementedException();
+        }
+        
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        public bool contains_token_with_key(string key)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
