b9f1382516edcd1d4f5ee17dd4df2d20fa51d729
Dimitri Holsteens <dimitri.holsteens@gmail.com>
2011-03-18 23:03:54 +0000

    Pushing new changes

--- product/nothinbutdotnetstore/utility/containers/basic/BasicDependencyFactory.cs
+++ product/nothinbutdotnetstore/utility/containers/basic/BasicDependencyFactory.cs
@@ -4,9 +4,16 @@ namespace nothinbutdotnetstore.utility.containers.basic
 {
     public class BasicDependencyFactory : DependencyFactory
     {
+        readonly Func<object> dependency_creation_expression;
+
+        public BasicDependencyFactory(Func<object> dependency_creation_expression)
+        {
+            this.dependency_creation_expression = dependency_creation_expression;
+        }
+
         public object create()
         {
-            throw new NotImplementedException();
+            return dependency_creation_expression();
         }
     }
 }
