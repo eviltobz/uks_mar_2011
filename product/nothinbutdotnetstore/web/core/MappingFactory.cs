d01125573ca37c15de0eaaf6a0b138d60b7d5905
Gareth Leachman <grleachman@yahoo.com>
2011-03-18 15:44:31 +0000

    Pushing new changes

---
+++ product/nothinbutdotnetstore/web/core/MappingFactory.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace nothinbutdotnetstore.web.core
+{
+    public interface MappingFactory
+    {
+        InputModelMapper<InputModel> create_for<InputModel>();
+    }
+}
