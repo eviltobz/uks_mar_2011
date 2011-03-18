4a4cdc7af607bd3ad1e293caa9f2528d6468675b
Peter Van den Daele <peter.vandendaele@gmail.com>
2011-03-18 22:46:24 +0000

    Pushing new changes

--- product/nothinbutdotnetstore/utility/containers/basic/DefaultDependenciesRegistry.cs
+++ product/nothinbutdotnetstore/utility/containers/basic/DefaultDependenciesRegistry.cs
@@ -6,15 +6,20 @@ namespace nothinbutdotnetstore.utility.containers.basic
     public class DefaultDependenciesRegistry : Dependencies
     {
         IDictionary<Type, DependencyFactory> all_factories;
+        MissingDependencyFactory missing_dependency_factory;
 
-        public DefaultDependenciesRegistry(IDictionary<Type, DependencyFactory> all_factories)
+        public DefaultDependenciesRegistry(IDictionary<Type, DependencyFactory> all_factories, MissingDependencyFactory missing_dependency_factory)
         {
             this.all_factories = all_factories;
+            this.missing_dependency_factory = missing_dependency_factory;
         }
 
         public DependencyFactory get_factory_that_can_create(Type dependency_type)
         {
-            return all_factories[dependency_type];
+            if (all_factories.ContainsKey(dependency_type))
+                return all_factories[dependency_type];
+            else
+                return this.missing_dependency_factory(dependency_type);
         }
     }
 }
