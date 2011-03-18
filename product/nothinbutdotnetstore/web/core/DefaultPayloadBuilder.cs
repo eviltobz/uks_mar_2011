56a2e4b44f0a15b194872e56a6968669f5f482e0
Dimitri Holsteens <dimitri.holsteens@gmail.com>
2011-03-18 10:51:51 +0000

    Pushing new changes

--- product/nothinbutdotnetstore/web/core/DefaultPayloadBuilder.cs
+++ product/nothinbutdotnetstore/web/core/DefaultPayloadBuilder.cs
@@ -5,9 +5,22 @@ namespace nothinbutdotnetstore.web.core
 {
     public class DefaultPayloadBuilder<ItemWithProperty> : PayloadBuilder<ItemWithProperty>
     {
+        readonly TokenStore token_store;
+        readonly ItemWithProperty item_with_property;
+        readonly PropertyExpressionTokenFactory token_factory;
+
+        public DefaultPayloadBuilder(TokenStore token_store, ItemWithProperty item_with_property,
+            PropertyExpressionTokenFactory token_factory)
+        {
+            this.token_store = token_store;
+            this.item_with_property = item_with_property;
+            this.token_factory = token_factory;
+        }
+
         public string with_detail<PropertyType>(Expression<PropertyAccessor<ItemWithProperty, PropertyType>> accessor)
         {
-            throw new NotImplementedException();
+            token_store.register(token_factory.create_from(accessor, item_with_property));
+            return string.Empty;
         }
     }
 }
