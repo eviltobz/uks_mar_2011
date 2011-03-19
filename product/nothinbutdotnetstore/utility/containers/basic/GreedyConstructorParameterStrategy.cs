36a7fe40275cb0edc2d9f6258eecc230427002f2
Peter Van den Daele <peter.vandendaele@gmail.com>
2011-03-19 00:39:48 +0000

    Pushing new changes

---
+++ product/nothinbutdotnetstore/utility/containers/basic/GreedyConstructorParameterStrategy.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Reflection;
+
+namespace nothinbutdotnetstore.utility.containers.basic
+{
+    public class GreedyConstructorParameterStrategy
+    {
+        public static ConstructorInfo get_the_greediest_constructor_info<item>()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
